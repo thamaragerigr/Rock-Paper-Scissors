3ca5d22ec1f4e637d2e1a09a5479a70ba15520cb
Thamara Gerig <thamaragerigr@gmail.com>
2019-11-17 21:57:29 +0100

    Estructura básica general

---
+++ js/script.js
@@ -0,0 +1,124 @@
+const game = ()=>{
+    let pScore = 0;
+    let cScore = 0;
+
+    //Comienzo del juego
+    const startGame = ()=>{
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () =>{
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+        });
+    };
+    //Partida
+    const playMatch = () =>{
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        const hands = document.querySelectorAll('.hands img');
+
+        hands.forEach(hand =>{
+           hand.addEventListener('animationend', function(){
+               this.style.animation = "";
+           });
+        });
+        //Opciones del oprdenador
+        const computerOptions = ['rock','paper','scissors'];
+
+        options.forEach((options)=>{
+              options.addEventListener('click', function() {
+                //Elección del ordenador  
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+
+                setTimeout(() => {
+                    compareHands(this.textContent, computerChoice);
+                    //Combio de imagenes
+                    playerHand.src = `./images/${this.textContent}.svg`;
+                    computerHand.src = `./images/${computerChoice}.svg`;
+    
+                }, 2000);
+
+                //animacion
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease";
+              });
+        });
+       
+    };
+
+    const updateScore = () =>{
+        const playerScore = document.querySelector('.player-score p');
+        const computerScore = document.querySelector('.computer-score p');
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    };
+
+    const compareHands = (playerChoice, computerChoice) =>{
+       const winner = document.querySelector('.winner'); 
+       //Si ocurre un empate
+      if(playerChoice === computerChoice){
+         winner.textContent = 'It is a tie';
+         return;
+      }
+
+      // Si elige piedra
+      if(playerChoice === 'rock'){
+          if(computerChoice === 'scissors'){
+              winner.textContent = 'Player wins';
+              updateScore();
+              pScore++;
+              return;
+          }else{
+              winner.textContent = 'Computer wins';
+              cScore++;
+              updateScore();
+              return;
+          }
+          
+      }
+
+      //Si elige papel
+      if(playerChoice === 'paper'){
+        if(computerChoice === 'scissors'){
+            winner.textContent = 'Computer wins';
+            cScore++;
+            updateScore();
+            return;
+        }else{
+            winner.textContent = 'Player wins';
+            pScore++;
+            updateScore();
+            return;
+        }
+        
+    }
+
+    //Si elige tijera
+    if(playerChoice === 'scissors'){
+        if(computerChoice === 'rock'){
+            winner.textContent = 'Computer wins';
+            cScore++;
+            updateScore();
+            return;
+        }else{
+            winner.textContent = 'Player wins';
+            pScore++;
+            updateScore();
+            return;
+        }  
+    }
+
+    }
+
+    //Llamada de las funcciones internas
+    startGame();
+    playMatch();
+};
+
+
+//Aqui comienza la funcción 'game'
+game();
